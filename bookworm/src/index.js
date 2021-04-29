0540139c8a76c1757d972b2c30781b419fb99e7b
srcrank <salmacrank@gmail.com>
2021-04-28 21:15:09 -0500

    auth start

--- bookworm/src/index.js
+++ bookworm/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Bookworm } from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router> 
-      <App />
+      <Bookworm />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
