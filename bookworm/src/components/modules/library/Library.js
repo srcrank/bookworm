34e27d20de30a679c3e3a8b383ccce8514c25ef8
srcrank <salmacrank@gmail.com>
2021-05-06 01:59:08 -0500

    more library bugs. useState is not updating correctly.

--- bookworm/src/components/modules/library/Library.js
+++ bookworm/src/components/modules/library/Library.js
@@ -1,44 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { LibraryCard } from './LibraryCard'
-import { getLibrary, deleteBookInLib, getAllBooks } from '../../../data/LibManager'
+import { getLibrary, deleteBookInLib } from '../../../data/LibManager'
 import { useHistory } from 'react-router-dom';
+import { getAllBooks, getBookById } from '../../../data/BookManager';
+
 
 export const Library = () => {
     const [libItems, setLib] = useState([]);
+    const [books, setBooks] = useState([]);
 
     const history = useHistory();
+    const currentUserId = parseInt(sessionStorage.getItem("bookworm_user"));
+
 
     const getUserLib = () => {
-       const allLibraries = getLibrary().then()
-       const userLibrary = 
-    }
-
-    const handleDeleteLibItem = (id) => {
-        deleteBookInLib(id)
-            .then(() => getAllBooks()
-                .then(setLib))
+      return getLibrary().then(res => res.filter(lib => lib.userId === currentUserId))
+       .then(userLib => setLib(userLib));
     };
 
+    const getBooksInLib = () => {
+        console.log('Start Get Books', libItems)
+        
+        libItems.map(libItem => {return getBookById(libItem.bookId).then(res => console.log("res", res))})
+    }
+
+    const handleDeleteLibItem = console.log("delete!")
+
     useEffect(() => {
-        // getLibrary();
+        getUserLib().then(getBooksInLib());
     }, []);
 
     return (
         <>
 
-            <section className="section-content">
+            {/* <section className="section-content">
                 <button type="button"
                     className="btn"
                     onClick={() => { history.push("/library/add") }}>
                     + book
                 </button>
-            </section>
+            </section> */}
 
 
-            {/* <div className="conatiner-cards">
-                {libItems.map(library =>
-                    <LibraryCard key={library.id} article={library} handleDeleteArticle={handleDeleteLibItem} />)}
-            </div> */}
+            <div className="container-libCards">
+                {books.map(book =>
+                    <LibraryCard 
+                    key={book.id} article={book} handleDeleteArticle={handleDeleteLibItem} />
+                    )}
+            </div>
         </>
     )
 };
