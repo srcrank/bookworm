ecee9a24dce41b0e537ac07f6de76550606fd91d
srcrank <salmacrank@gmail.com>
2021-05-09 14:52:36 -0500

    additional goal functionality! users can only see their goals. Users can edit and delete their goals. Users are prompted to create goals when they don't have any yet.

--- bookworm/src/components/modules/goals/GoalCard.js
+++ bookworm/src/components/modules/goals/GoalCard.js
@@ -8,45 +8,45 @@ export const GoalCard = ({ goal, handleDeleteGoal, handleCompleteGoal }) => {
   const currentUser = JSON.parse(sessionStorage.getItem("bookworm_user"));
 
   const history = useHistory();
-  return (
-    <div className="goal-card">
-      <div className="goalCard-content">
-        <h3>
-          <span className="goalCard-Name">{goal.name}</span>
-        </h3>
-        <p>Notes: {goal.description}</p>
-        {goal.isCompleted === true ? (
-          ""
-        ) : (
-          <p>Completion Goal: {goal.completion}</p>
-        )}
-        {goal.userId === currentUser && goal.isCompleted === false ? (
-          <button
-            type="button"
-            onClick={() => history.push(`/goals/${goal.id}/edit`)}
-          >
-            {" "}
-            ~ edit{" "}
-          </button>
-        ) : (
-          ""
-        )}
-        {goal.userId === currentUser ? (
-          <button type="button" onClick={() => handleDeleteGoal(goal.id)}>
-            {" "}
-            - delete
-          </button>
-        ) : (
-          ""
-        )}
-        {goal.userId === currentUser && goal.isCompleted === false ? (
-          <button type="button" onClick={() => handleCompleteGoal(goal)}>
-            Complete
-          </button>
-        ) : (
-          ""
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className="goal-card">
+          <div className="goalCard-content">
+            <h3>
+              <span className="goalCard-Name">{goal.name}</span>
+            </h3>
+            <p>Notes: {goal.description}</p>
+            {goal.isCompleted === true ? (
+              ""
+            ) : (
+              <p>Completion Goal: {goal.completion}</p>
+            )}
+            {goal.userId === currentUser && goal.isCompleted === false ? (
+              <button
+                type="button"
+                onClick={() => history.push(`/goals/${goal.id}/edit`)}
+              >
+                {" "}
+                ~ edit{" "}
+              </button>
+            ) : (
+              ""
+            )}
+            {goal.userId === currentUser ? (
+              <button type="button" onClick={() => handleDeleteGoal(goal.id)}>
+                {" "}
+                - delete
+              </button>
+            ) : (
+              ""
+            )}
+            {goal.userId === currentUser && goal.isCompleted === false ? (
+              <button type="button" onClick={() => handleCompleteGoal(goal)}>
+                Complete
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      );
 };
