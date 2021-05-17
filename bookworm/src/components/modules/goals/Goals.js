fb817850ae7f1eda32807b9a05d402e1ee14b8e4
srcrank <salmacrank@gmail.com>
2021-05-17 03:54:44 -0500

    finishing up some styling

--- bookworm/src/components/modules/goals/Goals.js
+++ bookworm/src/components/modules/goals/Goals.js
@@ -10,17 +10,30 @@ import {
   updateGoal,
 } from "../../../data/GoalManager";
 import { useHistory } from "react-router-dom";
-import "./Goals.css"
+import "./Goals.css";
 
 export const Goals = () => {
   const [goals, setGoals] = useState([]);
+  const [completedGoals, setcompletedGoals] = useState([]);
+  const [notCompletedGoals, setnotCompletedGoals] = useState([]);
 
   const history = useHistory();
 
   const currentUser = JSON.parse(sessionStorage.getItem("bookworm_user"));
 
   const getUserGoals = (currentUser) => {
+    const completeGoals = [];
+    const incompleteGoals = [];
     return getGoalByUser(currentUser).then((goalsFromAPI) => {
+      goalsFromAPI.forEach((goal) => {
+        if (goal.isCompleted) {
+          completeGoals.push(goal);
+        } else {
+          incompleteGoals.push(goal);
+        }
+      });
+      setcompletedGoals(completeGoals);
+      setnotCompletedGoals(incompleteGoals);
       setGoals(goalsFromAPI);
     });
   };
@@ -50,7 +63,7 @@ export const Goals = () => {
 
   return (
     <>
-    <section className="goalSection-content">
+      <section className="goalSection-content">
         <button
           type="button"
           className="goal-button"
@@ -61,20 +74,33 @@ export const Goals = () => {
           + add goal
         </button>
       </section>
-      {goals.length > 0 ? (
-        <div className="goalContainer-Cards">
-          {goals.map((goal) => (
-            <GoalCard
-              key={goal.id}
-              goal={goal}
-              handleCompleteGoal={handleCompleteGoal}
-              handleDeleteGoal={handleDeleteGoal}
-            />
-          ))}
+      {goals.length > 0 ? ( 
+        <div className="goalCard-Section"> 
+          <div className="goalContainer-Cards">
+            {completedGoals.map((completeGoal) => (
+              <GoalCard
+                key={completeGoal.id}
+                goal={completeGoal}
+                handleDeleteGoal={handleDeleteGoal}
+              />
+            ))}
+          </div>
+          <div className="goalIncompleteContainer-Cards">
+            {notCompletedGoals.map((incompleteGoal) => (
+              <GoalCard
+                key={incompleteGoal.id}
+                goal={incompleteGoal}
+                handleCompleteGoal={handleCompleteGoal}
+                handleDeleteGoal={handleDeleteGoal}
+              />
+            ))}
+          </div>
         </div>
       ) : (
         <NoGoalCard />
-      )}
+      )} 
+
+
     </>
   );
 };
